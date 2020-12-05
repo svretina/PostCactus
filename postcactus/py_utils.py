657dcd9619bef9f791a1c2e02bc3f427bc8c889b
Gabriele Bozzola <sbozzolator@gmail.com>
2020-12-05 08:28:12 -0700

    Speeed up apply_to_points

--- postcactus/py_utils.py
+++ postcactus/py_utils.py
@@ -23,7 +23,7 @@ several common operations in Python.
 import numpy as np
 
 
-def apply_to_points(function, array):
+def apply_to_points(function, array, num_dimensions, ufunc=False):
     """Apply a function on all the multidimensional points of array.
 
     Array can have any shape, but it is intended to be a collection
@@ -36,12 +36,59 @@ def apply_to_points(function, array):
     :type function: callable
     :param array: Collection of points
     :type array: numpy array
+    :param num_dimensions: Dimensionality of each point
+    :type num_dimensions: int
+    :param ufunc: function is a numpy universal function and can
+                  be applied at multiple points at the same time.
+    :type ufunc: bool
 
     :returns: function applied on each single point
     :rtype: numpy array
     """
+    # The case with 1D scalar data is tricky because it is not an array
+    input_is_scalar = not hasattr(array, "__len__")
 
-    return np.apply_along_axis(function, -1, np.asarray(array))
+    # Now we make sure we have an array. This does not change the dimensions
+    # in the case with 2 or more dimensions, but add one "level" to
+    # scalar inputs.
+    array_np = np.atleast_1d(array)
+
+    # We have to handle the case with only one point
+    input_is_one_point = array_np.shape == (num_dimensions, )
+
+    # With this we add one layer to the array, so for all the dimensions
+    # we have added one level when the input is a single point. We will
+    # strip this level later.
+    if input_is_one_point and not input_is_scalar:
+        array_np = np.asarray([array_np])
+
+    # The simplest way to achieve what we want is very slow, so we
+    # reserve that for cases that we cannot vectorize
+    if (not ufunc):
+        ret = np.apply_along_axis(function, -1, array_np)
+    else:
+        # In the case of universal functions we have to get our hands dirty to
+        # get massive speedups.
+        #
+        # We determine what is the shape of the points forgetting about
+        # their dimensionality (which we don't need). We use this reshape
+        # the output.
+        points_shape = array_np.shape[:-1]
+
+        # Next, we reshape up to the last axis, which means that
+        # now we have a collection of points
+        array_np = array_np.reshape(-1, array_np.shape[-1])
+        ret = function(array_np)
+        # Now we have to reconstruct the correct return shape.
+        # First, we determine what is the dimensionality of the output
+        # of function
+        shape_function_return = ret[0].shape
+        # And append this to the shape of the points
+        ret_shape = tuple(points_shape + shape_function_return)
+        # Finally, we reshape
+        ret = ret.reshape(ret_shape)
+
+    return ret[0] if input_is_one_point else ret
 
 
 def ndarray_to_tuple(ndarray):
--- tests/test_py_utils.py
+++ tests/test_py_utils.py
@@ -33,48 +33,106 @@ class TestPyUtils(unittest.TestCase):
             return 2 * x
 
         def scal_func(x):
-            return np.sum(x)
+            return np.sum(x, axis=-1)
 
-        # Only one point
-        self.assertTrue(
-            np.array_equal(put.apply_to_points(vec_func, [2, 3]), [4, 6])
-        )
+        # We run the same test suite with ufunc = Ture and ufunc = False
+        def test_ufunc(ufunc):
 
-        self.assertTrue(
-            np.array_equal(put.apply_to_points(scal_func, [2, 3]), 5)
-        )
+            # Only one point
+            self.assertTrue(
+                np.array_equal(
+                    put.apply_to_points(vec_func, [2, 3], 2, ufunc), [4, 6]
+                )
+            )
 
-        # Test two points
-        arr1 = [[2, 3], [10, 20]]
-        exp_arr1 = [[4, 6], [20, 40]]
-        self.assertTrue(
-            np.array_equal(put.apply_to_points(vec_func, arr1), exp_arr1)
-        )
+            self.assertTrue(
+                np.array_equal(
+                    put.apply_to_points(scal_func, [2, 3], 2, ufunc), 5
+                )
+            )
 
-        exp_arr1_scal = [5, 30]
-        self.assertTrue(
-            np.array_equal(put.apply_to_points(scal_func, arr1), exp_arr1_scal)
-        )
+            # Test two points
+            arr1 = [[2, 3], [10, 20]]
+            exp_arr1 = [[4, 6], [20, 40]]
+            self.assertTrue(
+                np.array_equal(
+                    put.apply_to_points(vec_func, arr1, 2, ufunc), exp_arr1
+                )
+            )
 
-        # Now three sets of three points
-        arr2 = [
-            [[2, 3], [10, 20], [-4, -6]],
-            [[5, 1], [1, 2], [8, 11]],
-            [[0, 1], [2, 3], [-1, -2]],
-        ]
-        exp_arr2 = [
-            [[4, 6], [20, 40], [-8, -12]],
-            [[10, 2], [2, 4], [16, 22]],
-            [[0, 2], [4, 6], [-2, -4]],
-        ]
-        self.assertTrue(
-            np.array_equal(put.apply_to_points(vec_func, arr2), exp_arr2)
-        )
+            exp_arr1_scal = [5, 30]
+            self.assertTrue(
+                np.array_equal(
+                    put.apply_to_points(scal_func, arr1, 2, ufunc),
+                    exp_arr1_scal,
+                )
+            )
 
-        exp_arr2_scal = [[5, 30, -10], [6, 3, 19], [1, 5, -3]]
-        self.assertTrue(
-            np.array_equal(put.apply_to_points(scal_func, arr2), exp_arr2_scal)
-        )
+            # Now three sets of three points
+            arr2 = [
+                [[2, 3], [10, 20], [-4, -6]],
+                [[5, 1], [1, 2], [8, 11]],
+                [[0, 1], [2, 3], [-1, -2]],
+            ]
+            exp_arr2 = [
+                [[4, 6], [20, 40], [-8, -12]],
+                [[10, 2], [2, 4], [16, 22]],
+                [[0, 2], [4, 6], [-2, -4]],
+            ]
+            self.assertTrue(
+                np.array_equal(
+                    put.apply_to_points(vec_func, arr2, 2, ufunc), exp_arr2
+                )
+            )
+
+            exp_arr2_scal = [[5, 30, -10], [6, 3, 19], [1, 5, -3]]
+            self.assertTrue(
+                np.array_equal(
+                    put.apply_to_points(scal_func, arr2, 2, ufunc),
+                    exp_arr2_scal,
+                )
+            )
+
+            # 1D data
+            # Scalar
+            arr3 = 3.5
+            self.assertEqual(put.apply_to_points(vec_func, arr3, 1, ufunc), 7)
+            self.assertTrue(
+                isinstance(
+                    put.apply_to_points(vec_func, arr3, 1, ufunc), np.float
+                )
+            )
+            # Vector
+            arr4 = [5.5]
+            self.assertTrue(
+                np.array_equal(
+                    put.apply_to_points(vec_func, arr4, 1, ufunc), [11]
+                )
+            )
+            self.assertTrue(
+                isinstance(
+                    put.apply_to_points(vec_func, arr4, 1, ufunc), np.ndarray
+                )
+            )
+            # Collection of scalars
+            arr5 = [1, 2, 3]
+            self.assertTrue(
+                np.array_equal(
+                    put.apply_to_points(vec_func, arr5, 1, ufunc), [2, 4, 6]
+                )
+            )
+            # Collection of "vectors"
+            arr6 = [[1], [2], [3]]
+            self.assertTrue(
+                np.array_equal(
+                    put.apply_to_points(vec_func, arr6, 1, ufunc),
+                    [[2], [4], [6]],
+                )
+            )
+
+        for ufunc in (False, True):
+            with self.subTest(ufunc=ufunc):
+                test_ufunc(ufunc)
 
     def test_ndarray_to_tuple(self):
 
